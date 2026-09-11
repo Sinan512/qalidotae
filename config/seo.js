0b5d7ff48a8016b02c95267f14ed96d13c8a5d8c
Sinan512 <hafizsinan512@gmail.com>
2026-09-11 21:56:01 +0530

    Update seo.js focused brand name by | sign

--- config/seo.js
+++ config/seo.js
@@ -3,7 +3,7 @@ module.exports = {
     siteName:"Qalidotae",
     brandName:"Qalidotae",
     canonicalUrl: "https://qalidotae-modern-arabic-fashion.vercel.app/",
-    defaultTitle:"Qalidotae-modern arabic fashion dresses & thobes",
-    defaultDescription:"Qalidotae modern thobe and abaya. Premium handcrafted Emirati, Moroccan, and Saudi garments, made with pure silk and spun cotton with worldwide Cash On Delivery.",
+    defaultTitle:"Qalidotae | modern arabic fashion dresses & thobes",
+    defaultDescription:"Qalidotae - modern thobe and abaya. Premium handcrafted Emirati, Moroccan, and Saudi garments, made with pure silk and spun cotton with worldwide Cash On Delivery.",
     defaultImage:"/logo.png"
 };
